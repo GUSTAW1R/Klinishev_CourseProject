e4660bf8fb3a4736daf1ae0a0553e0d3fe7ca2a1
dmitry <klinyshev.dmitrii@mail.ru>
2020-02-22 12:23:13 +0400

    auth_control

--- Klinishev_CourseProject_Controllers/AuthController.cs
+++ Klinishev_CourseProject_Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +13,15 @@ namespace Klinishev_CourseProject_Controllers
         //Путь измени, файл находится в папке Models
         private string sPath = Environment.CurrentDirectory + "\\DB.db";
         //Создадим объект от класса пользователей, он будет посредником в передачи данных
-        private Users users = new Users();
+        //private Users users = new Users();
 
         public AuthController()
         {
             string ConnectionString = @"Data Source=" + sPath + ";New=False;Version=3;DateTimeFormat=UnixEpoch";
             connection = new SQLiteConnection(ConnectionString);
             connection.Open();
-            this.Login = users.Login;
-            this.Password = users.Password;
+            //this.Login = users.Login;
+            //this.Password = users.Password;
         }
 
         public void check_validation(string Login, string Password)
